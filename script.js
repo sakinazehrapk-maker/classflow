8b489eedd57af09ee6e1e50bf0df906fc3b808a7
sakina <sakinazehrapk@gmail.com>
2026-07-05 19:04:11 +0500

    added inputs

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("UniFlow Started");
+const addBtn=document.getElementById("addClass");
+addBtn.addEventListener("click",()=>{
+    const course=document.getElementById("course").value;
+    const day=document.getElementById("day").value;
+    const start=document.getElementById("startTime").value;
+    const end=document.getElementById("endTime").value;
+    const room=document.getElementById("room").value;
+    console.log(course, day, start, end, room);
+});
